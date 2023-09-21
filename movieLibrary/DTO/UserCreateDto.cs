5118bbb0e97bb9dfed4eb5f3de89b87cf4e81973
fhers <fhers667@gmail.com>
2023-09-21 09:38:25 -0600

    user update

--- movieLibrary/DTO/UserCreateDto.cs
+++ movieLibrary/DTO/UserCreateDto.cs
@@ -10,7 +10,7 @@ namespace movieLibrary.DTO
         public DateTime? Birthdate { get; set; }        
         public string? Role { get; set; }
         public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
+        public string? Password { get; set; } = null!;
         public string? Token { get; set; }   
         public string? RefreshToken { get; set; }
         public DateTime RefreshTokenExpiryTime { get; set; }  
